0e5df867107ee27858ebb44664dc1c37afa67768
Adachi <ren@cps.im.dendai.ac.jp>
2017-01-06 15:02:26 +0900

    "multithreading of processing"

--- test.py
+++ test.py
@@ -2,52 +2,65 @@ from __future__ import print_function
 import csv
 import serial
 from time import sleep
+import time 
 from datetime import datetime
 import paho.mqtt.publish as publish
 import psutil
+import ssl
+import threading
 
-#serial port setting
-ser = serial.Serial('/dev/cu.usbmodem1411', 9600)
-smoothing = 100
-
-#thingspeak setting
-channelID = "210172"
-apiKey = "VCZEBMUTCTMLAQUT"
 useSSLWebsockets = True
 mqttHost = "mqtt.thingspeak.com"
-import ssl
 tTransport = "websockets"
 tTLS = {'ca_certs':"/etc/ssl/certs/ca-certificates.crt",'tls_version':ssl.PROTOCOL_TLSv1}
 tPort = 80 
-topic = "channels/" + channelID + "/publish/" + apiKey
 
-with open('airSensorLog.csv','w') as f:
-	try:
-	  while True:
-		time = datetime.now()
-		writer = csv.writer(f, lineterminator='\n')
-		value = ser.readline()
-		value = value.strip()
-		#(hcho, mq135, co, humi, temp,)
-		#(humi, temp) = value.split(",")
-		(hcho, mq135, co, humi, temp) = value.split(",")
-		#curl https://api.thingspeak.com/update/ -X POST -d field1=value  -H 'X-THINGSPEAKAPIKEY:LCDNR6WDV5MIFUUA'
-		a = [time, value, hcho,mq135,co,humi,temp] 
-		##writer.writerow(a)
-		print(a)
-		#cpuPercent = psutil.cpu_percent(interval=20)
-		#ramPercent = psutil.virtual_memory().percent
-		#print (" CPU =",cpuPercent,"   RAM =",ramPercent)
-		#tPayload = "field1=" + value
-		tPayload = "field1=" + hcho + "&field2=" + mq135 + "&field3=" + co + "&field4=" + humi + "&field5=" + temp 
-		try:
-			publish.single(topic, payload=tPayload, hostname=mqttHost,  transport=tTransport, port=tPort)
-			print ( "success to write")
-		except (KeyboardInterrupt):
-			break
-		except:
-			print ("There was an error while publishing the data.")
-	except KeyboardInterrupt:
-		f.close()
-		print("csv wrote")
-		pass
+class Sensors(threading.Thread):
+		#serial port setting
+
+	def __init__(self, pid, api, uport, filename):
+		self.id = pid
+		self.api = api
+		self.port = uport
+		self.filename = filename
+		threading.Thread.__init__(self)
+
+	def output(self):
+		ser = serial.Serial(self.port, 9600)
+		smoothing = 100
+
+		#thingspeak setting
+		channelID = self.id
+		apiKey = self.api 
+		topic = "channels/" + channelID + "/publish/" + apiKey
+
+		with open(self.filename ,'w') as f:
+			now = datetime.now()
+			writer = csv.writer(f, lineterminator='\n')
+			value = ser.readline()
+			value = value.strip()
+				#(hcho, mq135, co, humi, temp,)
+			(hcho, mq135, co, humi, temp) = value.split(",")
+			a = [now, value, hcho, mq135, co, humi, temp] 
+			print(a)
+			tPayload = "field1=" + hcho + "&field2=" + mq135 + "&field3=" + co + "&field4=" + humi + "&field5=" + temp 
+			try:
+				publish.single(topic, payload=tPayload, hostname=mqttHost,  transport=tTransport, port=tPort)
+				print ( "success to write")
+			except:
+			 	print ("there was an error while publishing the data.")
+try:
+	while True:
+		sensor1 = Sensors("210172", "VCZEBMUTCTMLAQUT", '/dev/cu.usbmodem14111', 'sensorlog1.csv')
+		sensor2 = Sensors("210278", "FW1031YQE7P84KL5", '/dev/cu.usbmodem14141', 'sensorlog2.csv')
+		sensor3 = Sensors("210283", "8ODO01FOLPIMHYY8", '/dev/cu.usbmodem14131', 'sensorlog3.csv')
+		sensor1.start()
+		sensor1.output()
+		sensor2.start()
+		sensor2.output()
+		sensor3.start()
+		sensor3.output()
+except KeyboardInterrupt:
+	print("finished")
+	pass
+
