547ebd1af3dc96fa93b8f874ee03c9dca42d043b
Adachi <ren@cps.im.dendai.ac.jp>
2017-01-21 15:48:45 +0900

    fix code

--- airSensorLog/airSensorLog.ino
+++ airSensorLog/airSensorLog.ino
@@ -1,15 +1,10 @@
-#include <COS_MQ7.h>
 #include "DHT.h"
-
-
-// Pin definitions
-#define ACTIVE_MONOX_PIN 5
-
-#define READ_MONOX_PIN A3
+#include <stdio.h>
 
 //hum&temp sensor
 #define DHTPIN 2  
 #define DHTTYPE DHT22
+
 
 // Variable initializations
 int reading;
@@ -18,53 +13,59 @@ int hcho_value;
 float h;
 float t;
 float f;
- 
-// Create CO sensor object
-// Last parameter is the duration of initial purge in seconds,
-// a negative value sets to purge to default: 500 seconds
-COS_MQ7 MQ7(ACTIVE_MONOX_PIN, READ_MONOX_PIN, -1);
+float hcho_R0 = 30.0;
+float hcho_ppm = 0;
+float value_adj = 49;
+
 DHT dht(DHTPIN, DHTTYPE);
 
-void setup() {
+void setup(){
   Serial.begin(9600);
   dht.begin();
 }
  
 void loop() {
-  //hcho, mq135, mq7, h, t, hic, hif
+  //hcho, mq135, h, t
   //HCHO sensor
   hcho_value = analogRead(A1);
-  float vol = hcho_value * 4.95/1023;
-  Serial.print(vol);
-  Serial.print(",");
+  //float vol = hcho_value * 4.95/1023;
+  float hcho_Rs = (1023.0/hcho_value)-1;
+  hcho_ppm = pow(10.0,((log10(hcho_Rs/hcho_R0)-0.0827)/(-0.4807)));
+  char hcho[12];
+  dtostrf(hcho_ppm,3,2,hcho);
+//  Serial.print(hcho_Rs);
+//  Serial.print(",");
 
   //Mq135 sensor
   mq135_value = analogRead(A0);
-  Serial.print(mq135_value, DEC);
-  Serial.print(",");
+  mq135_value = mq135_value + value_adj;
+//  Serial.print(mq135_value, DEC);
+//  Serial.print(",");
+  char mq135[12];
+  dtostrf(mq135_value,3,2,mq135);
 
-  //MQ7 sensor
-  MQ7.Power_cycle();
-  //for testing/info
-  Serial.print(MQ7.Get_current_CO_reading());
+  
 
   //humidity & tempareture sensor
   h = dht.readHumidity();
   t = dht.readTemperature();
   f = dht.readTemperature(true);
-  float hif = dht.computeHeatIndex(f, h);
-  float hic = dht.computeHeatIndex(t, h, false);
+//  float hif = dht.computeHeatIndex(f, h);
+//  float hic = dht.computeHeatIndex(t, h, false);
+  char temp[12];
+  dtostrf(t,3,2,temp);
+  char humi[12];
+  dtostrf(h,3,2,humi);
 
-  Serial.print(",");
-  Serial.print(h);
-  Serial.print(",");
-  Serial.println(t);
-  //Serial.print(", ");
-  //Serial.print(hic);
-  //Serial.print(", ");
-  //Serial.println(hif);
-
-  // wait a second (shouldn't be too much longer than 5-10 seconds)
+//  Serial.print(",");
+//  Serial.print(h);
+//  Serial.print(",");
+//  Serial.println(t);
+ 
+  char buf[44];
+  sprintf(buf, "%s,%s,%s,%s", hcho, mq135, humi, temp);
+  Serial.println(buf);
+  
   delay(1000);
 }
 
--- test.py
+++ test.py
@@ -7,60 +7,68 @@ from datetime import datetime
 import paho.mqtt.publish as publish
 import psutil
 import ssl
-import threading
+#import threading
+import io
 
 useSSLWebsockets = True
 mqttHost = "mqtt.thingspeak.com"
 tTransport = "websockets"
 tTLS = {'ca_certs':"/etc/ssl/certs/ca-certificates.crt",'tls_version':ssl.PROTOCOL_TLSv1}
 tPort = 80 
-
-class Sensors(threading.Thread):
+devices = ['/dev/ttyACM0', '/dev/ttyACM1', '/dev/ttyACM2']
+ser1 = serial.Serial(devices[0], 9600, timeout=2)
+ser2 = serial.Serial(devices[1], 9600, timeout=2)
+ser3 = serial.Serial(devices[2], 9600, timeout=2)
+class Sensors():
 		#serial port setting
 
-	def __init__(self, pid, api, uport, filename):
+	def __init__(self, pid, api, filename, ser):
 		self.id = pid
 		self.api = api
-		self.port = uport
 		self.filename = filename
-		threading.Thread.__init__(self)
+		self.ser = ser
+		#threading.Thread.__init__(self)
 
 	def output(self):
-		ser = serial.Serial(self.port, 9600)
-		smoothing = 100
-
+		sio = io.TextIOWrapper(io.BufferedRWPair(self.ser,self.ser), errors='ignore')
 		#thingspeak setting
 		channelID = self.id
 		apiKey = self.api 
 		topic = "channels/" + channelID + "/publish/" + apiKey
 
-		with open(self.filename ,'w') as f:
+		with open(self.filename ,'a') as f:
 			now = datetime.now()
 			writer = csv.writer(f, lineterminator='\n')
-			value = ser.readline()
-			value = value.strip()
-				#(hcho, mq135, co, humi, temp,)
-			(hcho, mq135, co, humi, temp) = value.split(",")
-			a = [now, value, hcho, mq135, co, humi, temp] 
-			print(a)
-			tPayload = "field1=" + hcho + "&field2=" + mq135 + "&field3=" + co + "&field4=" + humi + "&field5=" + temp 
-			try:
-				publish.single(topic, payload=tPayload, hostname=mqttHost,  transport=tTransport, port=tPort)
-				print ( "success to write")
-			except:
-			 	print ("there was an error while publishing the data.")
+
+			value = sio.readline()
+			print(self.id + "," +value)
+			list = value.split(",")
+			if(len(list) == 4):
+				(hcho, mq135, humi, temp) = value.split(",")
+				a = [now, hcho, mq135, humi, temp] 
+				writer.writerow(a)
+				tPayload = "field1=" + hcho + "&field2=" + mq135 + "&field4=" + humi + "&field5=" + temp 
+				try:
+					publish.single(topic, payload=tPayload, hostname=mqttHost,  transport=tTransport, port=tPort)
+					print ( "success to write")
+				except:
+			 		print ("there was an error while publishing the data.")
 try:
+	sensor1 = Sensors("210172", "vczebmutctmlaqut",  'sensorlog1.csv' , ser1) 
+	sensor2 = Sensors("210278", "fw1031yqe7p84kl5",  'sensorlog2.csv' , ser2) 
+	sensor3 = Sensors("210283", "8odo01folpimhyy8",  'sensorlog3.csv' , ser3) 
+
 	while True:
-		sensor1 = Sensors("210172", "VCZEBMUTCTMLAQUT", '/dev/cu.usbmodem14111', 'sensorlog1.csv')
-		sensor2 = Sensors("210278", "FW1031YQE7P84KL5", '/dev/cu.usbmodem14141', 'sensorlog2.csv')
-		sensor3 = Sensors("210283", "8ODO01FOLPIMHYY8", '/dev/cu.usbmodem14131', 'sensorlog3.csv')
-		sensor1.start()
 		sensor1.output()
-		sensor2.start()
+		ser1.open()
+		time.sleep(1)
 		sensor2.output()
-		sensor3.start()
+		ser2.open()
+		time.sleep(1)
 		sensor3.output()
+		ser3.open()
+		time.sleep(1)
+
 except KeyboardInterrupt:
 	print("finished")
 	pass
-
